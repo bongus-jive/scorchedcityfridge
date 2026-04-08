8c067f810eff661cbf973d4b63512c9175f02d00
patmanf <27874300+patmanf@users.noreply.github.com>
2026-04-07 22:19:07 -0500

    just add the resized fridge instead of copying it on top of every image. lol

--- pat_scorchedcityfridge.lua
+++ pat_scorchedcityfridge.lua
@@ -2,18 +2,15 @@ local assets_image, assets_add, fmt, find = assets.image, assets.add, string.for
 
 local fridge = assets_image("/objects/biome/scorchedcity/scorchedcityfridge/scorchedcityfridge.png")
 local width, height = table.unpack(fridge:size())
-local z = { 0, 0 }
-
 local files = assets.byExtension("png")
+
 for i = 1, #files do
   local file = files[i]
   if find(file, "^/dungeons/") or find(file, "^/ships/.+blocks%.png$") then goto continue end
 
-  local image = assets_image(file)
-  local size = image:size()
+  local size = assets_image(file):size()
   local scale = fmt("?scalenearest=%s;%s", size[1] / width, size[2] / height)
-  image:copyInto(z, fridge:process(scale))
-  assets_add(file, image)
+  assets_add(file, fridge:process(scale))
 
   ::continue::
 end
