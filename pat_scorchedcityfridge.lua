ca57b65d93887bedb2425ed29c845d9eca6bad80
patmanf <27874300+patmanf@users.noreply.github.com>
2026-04-07 19:50:31 -0500

    try to skip png dungeons & ship blockkeys

--- pat_scorchedcityfridge.lua
+++ pat_scorchedcityfridge.lua
@@ -3,9 +3,13 @@ local width, height = table.unpack(fridge:size())
 local z = { 0, 0 }
 
 for _, file in next, assets.byExtension("png") do
+  if file:find("^/dungeons/") or file:find("^/ships/.+blocks%.png$") then goto continue end
+
   local image = assets.image(file)
   local size = image:size()
   local scale = ("?scalenearest=%s;%s"):format(size[1] / width, size[2] / height)
   image:copyInto(z, fridge:process(scale))
   assets.add(file, image)
+
+  ::continue::
 end
