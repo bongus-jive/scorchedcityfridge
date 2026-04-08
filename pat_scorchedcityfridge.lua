c85c1869cf40cb508654012cfd44f51628d18631
patmanf <27874300+patmanf@users.noreply.github.com>
2026-04-07 20:12:59 -0500

    localtuah

--- pat_scorchedcityfridge.lua
+++ pat_scorchedcityfridge.lua
@@ -1,15 +1,19 @@
-local fridge = assets.image("/objects/biome/scorchedcity/scorchedcityfridge/scorchedcityfridge.png")
+local assets_image, assets_add, fmt, find = assets.image, assets.add, string.format, string.find
+
+local fridge = assets_image("/objects/biome/scorchedcity/scorchedcityfridge/scorchedcityfridge.png")
 local width, height = table.unpack(fridge:size())
 local z = { 0, 0 }
 
-for _, file in next, assets.byExtension("png") do
-  if file:find("^/dungeons/") or file:find("^/ships/.+blocks%.png$") then goto continue end
+local files = assets.byExtension("png")
+for i = 1, #files do
+  local file = files[i]
+  if find(file, "^/dungeons/") or find(file, "^/ships/.+blocks%.png$") then goto continue end
 
-  local image = assets.image(file)
+  local image = assets_image(file)
   local size = image:size()
-  local scale = ("?scalenearest=%s;%s"):format(size[1] / width, size[2] / height)
+  local scale = fmt("?scalenearest=%s;%s", size[1] / width, size[2] / height)
   image:copyInto(z, fridge:process(scale))
-  assets.add(file, image)
+  assets_add(file, image)
 
   ::continue::
 end
